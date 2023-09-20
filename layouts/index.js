3ee33e2b59806b389495330e1bb6bba05d6db5c3
kavikkannan <kavikkannan.k@gmail.com>
2023-09-21 00:33:53 +0530

    final

---
+++ layouts/index.js
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+export const Layout = (props) => {
+    <div>
+        <Link href={`/home`}></Link>
+        <Link href={`/register`}></Link>
+        <Link href={`/login`}></Link>
+        <Link href={`/q1`}></Link>
+        <Link href={`/q2`}></Link>
+    </div>
+}
